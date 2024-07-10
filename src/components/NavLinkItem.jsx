b1fb1abf225b04ce0a76ab9ed0d5341943376c54
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-10 11:44:50 -0600

    added react icons, added transitions for the nav elemtents being active

--- src/components/NavLinkItem.jsx
+++ src/components/NavLinkItem.jsx
@@ -7,8 +7,8 @@ const NavLinkItem = ({ to, children }) => {
       to={to}
       className={({ isActive }) =>
         isActive
-          ? 'bg-primary text-secondary rounded px-4 py-2'
-          : 'hover:text-accent px-4 py-2'
+          ? 'bg-primary text-secondary rounded-3xl px-4 py-2 transition-all duration-300'
+          : 'hover:text-accent px-4 py-2 transition-all duration-300'
       }
     >
       {children}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavLinkItem from './NavLinkItem';
+import '../cssStyles/Navbar.css'; // Import the CSS file here
+
 
 function Navbar() {
   return (
     <nav className="bg-secondary p-2">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className='flex gap-2 items-center text-primary'>
-          <img className="w-6 md:w-14" src={process.env.PUBLIC_URL + '/logos/Glass_Gecko_Games_Icon_clear.png'} alt="Logo" />
+          <img className="w-10 md:w-14" src={process.env.PUBLIC_URL + '/logos/Glass_Gecko_Games_Icon_clear.png'} alt="Logo" />
           <h1 className="text-xl">Glass Gecko Games</h1>
         </Link>
         <ul className="flex space-x-4 text-primary">
