cd187c7d025c863a047c61be2c48389cbf1f1d28
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-10 13:02:48 -0600

    set up better nav bar transitions

--- src/components/NavLinkItem.jsx
+++ src/components/NavLinkItem.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavLinkItem = ({ to, children }) => {
+
+ const NavLinkItem = ({ to, children, onClick }) => {
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
-        isActive
-          ? 'bg-primary text-secondary rounded-3xl px-4 py-2 transition-all duration-300'
-          : 'hover:text-accent px-4 py-2 transition-all duration-300'
+        `${isActive ? 'bg-primary text-secondary rounded-3xl' : 
+        'hover:text-accent bg-none duration-300'
+        } 
+        px-4 py-2 transition-all `
       }
+      onClick={onClick}
     >
       {children}
     </NavLink>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {useState} from 'react';
 import NavLinkItem from './NavLinkItem';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import '../cssStyles/Navbar.css'; // Import the CSS file here
 
 function Navbar() {
-  const [isHambugerNavOpen, setIsHamburgerNavOpen] = useState(false);
+  const [isHamburgerNavOpen, setIsHamburgerNavOpen] = useState(false);
+  const [animateMenu, setAnimateMenu] = useState(false);
+
+  const toggleMenu = () => {
+    if (!isHamburgerNavOpen) {
+      setIsHamburgerNavOpen(true);
+      setAnimateMenu(true);
+    } else {
+      setAnimateMenu(false);
+      setTimeout(() => setIsHamburgerNavOpen(false), 300); // Match this duration to your CSS animation duration
+    }
+  };
 
   return (
     <nav className="bg-secondary p-2">
@@ -15,7 +25,6 @@ function Navbar() {
           <img className="w-10 md:w-14" src={process.env.PUBLIC_URL + '/logos/Glass_Gecko_Games_Icon_clear.png'} alt="Logo" />
           <h1 className="text-xl">Glass Gecko Games</h1>
         </Link>
-
 
         <ul className="hidden space-x-4 text-primary md:flex">
           <li>
@@ -32,27 +41,27 @@ function Navbar() {
           </li>
         </ul>
 
-        <button onClick={()=>{setIsHamburgerNavOpen(!isHambugerNavOpen)}} className="text-primary block md:hidden z-20">
-          {isHambugerNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        <button onClick={toggleMenu} className="text-primary block md:hidden z-20">
+          {isHamburgerNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
 
-        {isHambugerNavOpen && (
-          <div className=" md:hidden  fixed top-0 left-0 z-10 w-full h-full bg-gray-900 bg-opacity-60 flex justify-center items-center">
+        {isHamburgerNavOpen && (
+          <div className={`fixed md:hidden top-0 left-0 z-10 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center transition-opacity duration-300 ${animateMenu ? 'opacity-100 fade-in' : 'opacity-0 fade-out'}`}>
             <ul className="text-white text-2xl space-y-4 text-center">
               <li>
-                <NavLinkItem to="/">Home</NavLinkItem>
+                <NavLinkItem to="/" onClick={toggleMenu}>Home</NavLinkItem>
               </li>
               <li>
-                <NavLinkItem to="/about">About Us</NavLinkItem>
+                <NavLinkItem to="/about" onClick={toggleMenu}>About Us</NavLinkItem>
               </li>
               <li>
-                <NavLinkItem to="/news">News</NavLinkItem>
+                <NavLinkItem to="/news" onClick={toggleMenu}>News</NavLinkItem>
               </li>
               <li>
-                <NavLinkItem to="/contact">Contact Us</NavLinkItem>
+                <NavLinkItem to="/contact" onClick={toggleMenu}>Contact Us</NavLinkItem>
               </li>
             </ul>
-          </div>  
+          </div>
         )}
       </div>
     </nav>
