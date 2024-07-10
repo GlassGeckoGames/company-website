822ca7f2b645ea6a74ca416b136b403bf8149cc1
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-10 12:00:29 -0600

    have hamburger menu working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useState} from 'react';
 import NavLinkItem from './NavLinkItem';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import '../cssStyles/Navbar.css'; // Import the CSS file here
 
-
 function Navbar() {
+  const [isHambugerNavOpen, setIsHamburgerNavOpen] = useState(false);
+
   return (
     <nav className="bg-secondary p-2">
       <div className="container mx-auto flex justify-between items-center">
@@ -12,7 +15,9 @@ function Navbar() {
           <img className="w-10 md:w-14" src={process.env.PUBLIC_URL + '/logos/Glass_Gecko_Games_Icon_clear.png'} alt="Logo" />
           <h1 className="text-xl">Glass Gecko Games</h1>
         </Link>
-        <ul className="flex space-x-4 text-primary">
+
+
+        <ul className="hidden space-x-4 text-primary md:flex">
           <li>
             <NavLinkItem to="/">Home</NavLinkItem>
           </li>
@@ -26,6 +31,29 @@ function Navbar() {
             <NavLinkItem to="/contact">Contact Us</NavLinkItem>
           </li>
         </ul>
+
+        <button onClick={()=>{setIsHamburgerNavOpen(!isHambugerNavOpen)}} className="text-primary block md:hidden z-20">
+          {isHambugerNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+
+        {isHambugerNavOpen && (
+          <div className=" md:hidden  fixed top-0 left-0 z-10 w-full h-full bg-gray-900 bg-opacity-60 flex justify-center items-center">
+            <ul className="text-white text-2xl space-y-4 text-center">
+              <li>
+                <NavLinkItem to="/">Home</NavLinkItem>
+              </li>
+              <li>
+                <NavLinkItem to="/about">About Us</NavLinkItem>
+              </li>
+              <li>
+                <NavLinkItem to="/news">News</NavLinkItem>
+              </li>
+              <li>
+                <NavLinkItem to="/contact">Contact Us</NavLinkItem>
+              </li>
+            </ul>
+          </div>  
+        )}
       </div>
     </nav>
   );
