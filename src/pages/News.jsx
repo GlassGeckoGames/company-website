7eaf2629d48e849b052ef869adc0f961a58e7658
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-07-11 13:08:47 -0600

    reset the news page and added more height to the tailwind config, added Newscard.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -14,42 +14,40 @@
  * 
  * @author Chace Nielson
  * @created 2024-07-10
- * @updated 2024-07-10
+ * @updated 2024-07-11
  */
-
 import React from 'react';
 import { newsItems } from '../data/newsItems';
+import NewsCard from '../components/NewsCard';
 
 function News() {
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-secondary">Our Latest Updates</h1>
-      <ul className="mt-8 space-y-4">
-        {newsItems.map((news, index) => (
-          <li key={index} className="border-b pb-4 flex flex-col md:flex-row md:items-center">
-            {news.image && (
-              <div className="md:w-1/3 mt-4 md:mt-0">
-                <img src={process.env.PUBLIC_URL + `/${news.image}`} alt={news.title} className="w-full h-auto" />
-              </div>
-            )}
-            <div className="md:flex-1 md:ml-4">
-              <h2 className="text-2xl font-bold text-primary">{news.title}</h2>
-              <p className="text-sm text-gray-500">{news.date} - {news.category}</p>
-              <p className="mt-2">{news.description}</p>
-              {news.link && (
-                <a
-                  href={news.link.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-accent hover:text-accent-dark mt-2 inline-block"
-                >
-                  {news.link.name}
-                </a>
-              )}
-            </div>
-          </li>
+    <div className="container mx-auto p-4 h-full">
+      <h1 className="text-3xl font-bold text-secondary mb-4">Latest Updates</h1>
+
+      {/* Headline news item */}
+      <div>
+        <NewsCard
+          key={-1}
+          news={newsItems[0]}
+          isHeadline={true}
+        />
+      </div>
+
+      {/* Separator line */}
+      <div className='h-2 bg-secondary my-4 rounded-lg'></div>
+
+      {/* List of other news items */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {newsItems.slice(1).map((news, index) => (
+          <NewsCard
+            key={index}
+            news={news}
+            isHeadline={false}
+          />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
