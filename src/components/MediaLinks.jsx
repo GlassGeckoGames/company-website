9868047b428c8dcee6614076b8fb1f18e8797e03
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-08-27 21:14:28 -0600

    added itch.io media link

--- src/components/MediaLinks.jsx
+++ src/components/MediaLinks.jsx
@@ -19,20 +19,8 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { FaYoutube, FaInstagram, FaTiktok, FaLinkedin, FaSteam } from 'react-icons/fa';
+import { FaYoutube, FaInstagram, FaTiktok, FaLinkedin, FaSteam, FaItchIo } from 'react-icons/fa';
 
-// Create a React component for the Twitter SVG
-// const TwitterIcon = () => (
-//   <svg
-//     fill="currentColor"
-//     xmlns="http://www.w3.org/2000/svg"
-//     viewBox="0 0 50 50"
-//     width="28px"
-//     height="28px"
-//   >
-//     <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"/>
-//   </svg>
-// );
 
 const Tooltip = ({ text }) => (
   <div className="absolute z-10 p-1 text-sm text-secondary bg-primary rounded shadow-lg bottom-full mb-2">
@@ -56,7 +44,7 @@ function MediaLinks() {
     { name: "TikTok", icon: FaTiktok, link: "https://www.tiktok.com/@glassgeckogames" },
     { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/glassgeckogames/" },
     { name: "YouTube", icon: FaYoutube, link: "https://www.youtube.com/@GlassGeckoGames" },
-    // { name: "Twitter", icon: TwitterIcon, link: "https://x.com/GlassGeckoGames" }, // Custom handling for Twitter
+    { name: "Itch.io", icon: FaItchIo, link: "https://glass-gecko-games.itch.io/" },
     { name: "LinkedIn", icon: FaLinkedin, link: "https://www.linkedin.com/company/glass-gecko-games/" },
     { name: "Steam", icon: FaSteam, link: "https://store.steampowered.com/search/?developer=Glass%20Gecko%20Games" },
   ];
